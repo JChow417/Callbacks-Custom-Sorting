02a2a0b4c8aa6949bb15bcde25dfad359bad1f89
John Chow <JChow417@gmail.com>
2016-09-08 18:16:57 +0000

    Completed assignment

---
+++ advanced-sorting.js
@@ -0,0 +1,19 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(student1, student2) {
+  if( student1.name < student2.name) {
+    return -1;
+  } else if ( student1.name > student2.name) {
+    return 1;
+  } else {
+    return student2.age - student1.age;
+  }
+
+})
+
+console.log(students);
